ff10232a1360298c9d25e754c65bca1247481066
Tomek Regulski <tomek.regulski@gmail.com>
2022-04-17 23:21:46 -0400

    add routes

---
+++ server/src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+  getTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+} from '../controllers/todos/index';
+
+const router: Router = Router();
+
+router.get('/todos', getTodos);
+router.post('/todos', addTodo);
+router.put('/todos/:id', updateTodo);
+router.delete('/todos/:id', deleteTodo);
+
+export default router;
